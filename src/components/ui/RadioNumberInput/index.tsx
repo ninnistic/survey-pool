d3dbdda592f9f49a039abaff573d098a425c7e76
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-02-22 15:04:14 +0900

    Create RadioNumberInput with dynamic generation - #10

--- src/components/ui/RadioNumberInput/index.tsx
+++ src/components/ui/RadioNumberInput/index.tsx
@@ -1,3 +1,55 @@
-export default function RadioNumberInput() {
-  return <input type="number" />;
+import { PlaceholderType } from "../../../types/questions";
+import { useId, useState, ComponentProps } from "react";
+
+export default function RadioNumberInput({
+  placeholder,
+}: RadioNumberInputProps) {
+  const [value, setValue] = useState(
+    typeof placeholder === "number" ? placeholder : 0
+  );
+
+  // dynamically generate radio buttons
+  const buttons = Array(7)
+    .fill(0)
+    .map((_, i) => (
+      <CustomRadioButton
+        key={i}
+        value={i + 1}
+        label={`${i + 1}`}
+        checked={i + 1 === value}
+        onChange={() => setValue(i + 1)}
+      />
+    ));
+
+  return <>{buttons}</>;
+  // return (
+  //   <>
+  //     <CustomRadioButton value="1" label="1" />
+  //     <CustomRadioButton value="2" label="2" />
+  //     <CustomRadioButton value="3" label="3" />
+  //     <CustomRadioButton value="4" label="4" />
+  //     <CustomRadioButton value="5" label="5" />
+  //     <CustomRadioButton value="6" label="6" />
+  //     <CustomRadioButton value="7" label="7" />
+  //   </>
+  // );
 }
+
+type RadioNumberInputProps = {
+  placeholder: PlaceholderType;
+};
+
+export function CustomRadioButton({ label, ...other }: CustomRadioButtonProps) {
+  const id = useId();
+
+  return (
+    <>
+      <input type="radio" name="number" {...other} />
+      <label htmlFor={id}>{label}</label>
+    </>
+  );
+}
+type CustomRadioButtonProps = {
+  label: string;
+  placeholder?: PlaceholderType;
+} & ComponentProps<"input">;
