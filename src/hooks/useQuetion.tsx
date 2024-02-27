999146c974cfb5774e46bbd3392badb8fc9a2918
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-02-27 16:10:06 +0900

    Change custom hook name - #17

---
+++ src/hooks/useQuetion.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { FormData } from "../types/questions";
+import { EscapeValidateRule } from "../types/questions";
+
+export const useQuestion = (url: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [questionList, setQuestionList] = useState<FormData[]>([]);
+  const [escapeValidateRule, setEscapeValidateRule] =
+    useState<EscapeValidateRule>();
+
+  useEffect(() => {
+    const fetchQuestionList = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(url);
+        const initialData = await response.json();
+        const forms = initialData.data.forms;
+        const escapeValidate = initialData.data.escapeValidate;
+        setQuestionList(forms);
+        setEscapeValidateRule(escapeValidate);
+      } catch (error) {
+        setIsLoading(false);
+      }
+    };
+    fetchQuestionList();
+  }, [url]);
+
+  return { isLoading, questionList, escapeValidateRule };
+};
