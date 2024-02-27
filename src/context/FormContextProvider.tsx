3d6096e12aafe5955b4f30aab448aa0c2c8c8a9f
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-02-27 16:12:02 +0900

    Refactor FromContextProvider with Reducer - #17

--- src/context/FormContextProvider.tsx
+++ src/context/FormContextProvider.tsx
@@ -1,15 +1,20 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 
 const FormContext = createContext<ContextProps | {}>({});
-const FormContextProvider = ({ children }: FormProviderProps) => {
-  const [tab, setTab] = useState(0);
-
-  const handleClickForward = () => {
-    setTab(tab + 1);
-  };
+const FormContextProvider = ({
+  children,
+  questionCount,
+}: FormProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, {
+    tab: 0,
+    hasPrev: false,
+    hasNext: true,
+    questionCount,
+  });
+  console.log(state);
 
   return (
-    <FormContext.Provider value={{ tab, setTab, handleClickForward }}>
+    <FormContext.Provider value={{ state, dispatch }}>
       {children}
     </FormContext.Provider>
   );
@@ -19,11 +24,39 @@ export default FormContextProvider;
 // FormContext에서 가져온 값을 ContextProps 타입으로 변환하여 반환하는 함수
 export const useFormContext = () => useContext(FormContext) as ContextProps;
 
+function reducer(state: State, action: Action): State {
+  switch (action) {
+    case "next":
+      return {
+        ...state,
+        tab: state.tab < state.questionCount - 1 ? state.tab + 1 : state.tab,
+        hasNext: state.tab < state.questionCount - 1 ? true : false,
+        hasPrev: state.tab > 0 ? true : false,
+      };
+    case "prev":
+      return {
+        ...state,
+        tab: state.tab > 0 ? state.tab - 1 : state.tab,
+        hasNext: state.tab < state.questionCount - 1 ? true : false,
+        hasPrev: state.tab > 0 ? true : false,
+      };
+    default:
+      return state;
+  }
+}
+
 type FormProviderProps = {
-  children: JSX.Element;
+  children: React.ReactNode;
+  questionCount: number;
 };
 type ContextProps = {
+  state: State;
+  dispatch: React.Dispatch<Action>;
+};
+type Action = "next" | "prev";
+type State = {
   tab: number;
-  setTab: React.Dispatch<React.SetStateAction<number>>;
-  handleClickForward: () => void;
+  hasPrev: boolean;
+  hasNext: boolean;
+  questionCount: number;
 };
