775f7ce47b3ccaa96f74d93c84f3d21ef8580d39
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-02-27 16:11:05 +0900

    Refactor FormContextProvider inside of SurveyForm - #17

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import "./App.css";
-import { useQuestion } from "./hooks/useCommonQuetion";
+import { useQuestion } from "./hooks/useQuetion";
 import SurveyForm from "./components/SurveyForm";
-import FormContextProvider from "./context/FormContextProvider";
+
 function App() {
   const COMMON_QUESTION_URL = "api/question/common";
   const { questionList } = useQuestion(COMMON_QUESTION_URL);
 
   // 어떻게 하면 순서대로 input창을 보여줄지 생각하기
   return (
-    <FormContextProvider>
-      <div className="App">
-        <SurveyForm questionList={questionList} action="#" />
-      </div>
-    </FormContextProvider>
+    <div className="App">
+      <SurveyForm questionList={questionList} action="#" />
+    </div>
   );
 }
 
