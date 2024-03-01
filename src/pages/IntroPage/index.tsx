d23271510d54de48262d85e3822d9722dcaa279f
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-03-01 18:10:29 +0900

    Create IntroPage - #24

--- src/pages/IntroPage/index.tsx
+++ src/pages/IntroPage/index.tsx
@@ -1,3 +1,13 @@
+import { useNavigate } from "react-router-dom";
 export default function IntroPage() {
-  return <div>IntroPage</div>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/survey");
+  };
+  return (
+    <div>
+      IntroPage
+      <button onClick={handleClick}>Start</button>
+    </div>
+  );
 }
